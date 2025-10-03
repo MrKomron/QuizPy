d62aecc60bc91106d2d75a73380e705ad46388ce
MrKomron <vosidov.komron@gmail.com>
2025-10-03 23:22:37 +0200

    first setup

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+import sys
+from PyQt6.QtWidgets import QApplication, QLabel, QWidget
 
+app = QApplication(sys.argv)
+window = QWidget()
+window.setWindowTitle("PyQt6 Test")
+window.setGeometry(800, 800, 300, 200)
+
+label = QLabel("Hello PyQt6!", parent=window)
+label.move(100, 80)
+
+window.show()
+sys.exit(app.exec())
